4dd49f63c7837085f6f20ab9fe456ed0c7b2db3a
ElisaMadsen <e.leisa@hotmail.com>
2021-09-16 13:05:39 +0200

    Committ 16-09-2021 13:05

--- src/ØdeØAflevering.java
+++ src/ØdeØAflevering.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class ØdeØAflevering {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 //Welcome to user
@@ -8,7 +9,7 @@ public class ØdeØAflevering {
 
 //10 options for user
         String[] options = {"1. tent", "2. alcohol", "3. knife", "4. gameboy", "5. sewingkit", "6. harmonica", "7. music", "8. matches", "9. book", "10. pet"};
-
+        int [] optionsIndex = new int[10];
 //Printing the options for user to choose from
         for (String option : options) {
             System.out.println(option);
@@ -18,16 +19,45 @@ public class ØdeØAflevering {
 
 //Kan ændres til while loop, hvis der er tid.
 //Det er så useren kan give ti input, også hvis man svarer forkert
-        for (int i = 1; i <= options.length; i++) {
+        for (int i = 1; i <= 10; i++) {
             int userInputs = scanner.nextInt();
             System.out.println(i);
+
+            optionsIndex[userInputs-1] ++;
 
             if (userInputs > options.length) {
                 System.out.println("Your choice is not available");
             }
         }
-//
+//Putting inputs from user into an int array
+        for (int option : optionsIndex) {
+            System.out.print(option + ", ");
+        }
+        System.out.println();
 
+        int maximum = optionsIndex[0];
+        int mostChosenElement = 0;
+        for (int i = 0; i < options.length; i++) {
+            int currentElement = optionsIndex[i];
+            if (currentElement > maximum){
+                maximum = currentElement;
+                mostChosenElement = i;
+            }
+        }
+//If two items have the same amount of votes, the console will show both elements
+        String moreThanTwoChosenElements = "";
+
+        int mostChosenValue = optionsIndex[mostChosenElement];
+
+        for (int i = 0; i < options.length; i++) {
+            if (mostChosenValue == optionsIndex[i]){
+                moreThanTwoChosenElements += options[i] + ", ";
+            }
+        }
+
+        System.out.println("\n" + moreThanTwoChosenElements + "is the most frequent item(s) that was chosen");
+
+        }
 
     }
-}
+
